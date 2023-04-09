b268ac47918eeb28d92bfcea77d6578f39eb1ff7
komorebi <984683402@qq.com>
2023-04-09 16:10:16 +0800

    第一次提交

---
+++ src/ui/LoginJFrame.java
@@ -0,0 +1,20 @@
+package ui;
+
+import javax.swing.*;
+
+public class LoginJFrame extends JFrame {
+    public LoginJFrame(){
+        //窗口的宽高
+        this.setSize(488,430);
+        //界面标题
+        this.setTitle("登录");
+        //界面居中
+        this.setLocationRelativeTo(null);
+        //设置关闭模式
+        this.setDefaultCloseOperation(3);
+
+
+        //是否可见
+        this.setVisible(true);
+    }
+}
---
+++ src/ui/RegisterJFrame.java
@@ -0,0 +1,18 @@
+package ui;
+
+import javax.swing.*;
+
+public class RegisterJFrame extends JFrame {
+    public RegisterJFrame(){
+        this.setSize(488,500);
+        //界面标题
+        this.setTitle("注册");
+        //界面居中
+        this.setLocationRelativeTo(null);
+        //设置关闭模式
+        this.setDefaultCloseOperation(3);
+
+
+        this.setVisible(true);
+    }
+}
